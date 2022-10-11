32e7b5b7fd5d054d4c153e0b092f531b7c81e536
Cris <cjgalvisc@unal.edu.co>
2022-10-11 23:41:14 +0200

    BlackJack Laboratory Smell

--- blackjack/assets/js/game.js
+++ blackjack/assets/js/game.js
@@ -32,6 +32,8 @@ const getCardValue = (card) => {
            parseInt(cardValue)
 }
 
+
+
 // Main
 let deck = [];
 deck = createDeck(deck=deck);
@@ -40,10 +42,80 @@ let computerScore = 0;
 
 // Events
 const btnRequestCard = document.querySelector("#btnRequestCard") ;
-const smallplayersScore = document.querySelectorAll("small");
+const btnStop = document.querySelector("#btnStop");
+const btnNewGame = document.querySelector("#btnNewGame ");
+
+const smallPlayersScore = document.querySelectorAll("small");
+const divPlayerCards = document.querySelector("#cards-player");
+const divComputerCards = document.querySelector("#cards-computer");
+
+const computerTurn = (minimalPoints) => {
+    do {
+        const requestedCard = requestCard(deck=deck);
+        computerScore += getCardValue(card=requestedCard);
+        smallPlayersScore[1].innerText = computerScore;
+    
+        const imgCard = document.createElement("img");
+        imgCard.src = `assets/cards/${requestedCard}.png`;
+        imgCard.classList.add('cards');
+        divComputerCards.append(imgCard);
+        if(minimalPoints > 21){
+            break;
+        }
+    } while((computerScore < minimalPoints) && (minimalPoints) <= 21)
+
+    setTimeout(()=>{
+        if (computerScore === minimalPoints){
+            alert("Nobody Win!!");
+        } else if(minimalPoints>21){
+            alert("COMPUTER Win!!");
+        } else if(computerScore>21){
+            alert("PLAYER Win!!");
+        }else{
+            alert("COMPUTER Win!!");
+        }
+    }, 10);
+    
+}
 
 btnRequestCard.addEventListener("click", ()=>{
     const requestedCard = requestCard(deck=deck);
     playerScore += getCardValue(card=requestedCard);
-    smallplayersScore[0].innerText = playerScore;
+    smallPlayersScore[0].innerText = playerScore;
+
+    const imgCard = document.createElement("img");
+    imgCard.src = `assets/cards/${requestedCard}.png`;
+    imgCard.classList.add('cards');
+    divPlayerCards.append(imgCard);
+
+    if(playerScore>21){
+        btnRequestCard.disabled = true;
+        btnStop.disabled = true;
+        computerTurn(minimalPoints=playerScore);
+    }else if(playerScore === 21){
+        btnRequestCard.disabled = true;
+        btnStop.disabled = true;
+        computerTurn(minimalPoints=playerScore);
+    }
+})
+
+btnStop.addEventListener("click", ()=>{
+    btnRequestCard.disabled = true;
+    btnStop.disabled = true;
+    computerTurn(minimalPoints=playerScore);
+})
+
+btnNewGame.addEventListener("click", ()=>{
+    deck = [];
+    deck = createDeck(deck=deck);
+    playerScore = 0;
+    computerScore = 0;
+
+    btnRequestCard.disabled = false;
+    btnStop.disabled = false;
+
+    smallPlayersScore[0].innerText = 0;
+    smallPlayersScore[1].innerText = 0;
+    divPlayerCards.innerHTML = "";
+    divComputerCards.innerHTML = "";
 })
